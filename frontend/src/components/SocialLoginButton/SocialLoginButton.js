9af4587a6bf3a061029814b2c2c4e609ef2c2ef7
HaeMin Yoon <49224104+Yoon-Hae-Min@users.noreply.github.com>
2022-05-02 16:30:17 +0900

    :twisted_rightwards_arrows: SocialLoginButton 컴포넌트 UI구현 (#12)
    * :lipstick: SocialLoginButton 컴포넌트 UI구현
    
    각 아이콘에 따른 svg 파일추가
    eslint-plugin-better-styled-components 플러그인 추가
    
    * :fire: default porptpyes 제거

---
+++ frontend/src/components/SocialLoginButton/SocialLoginButton.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import * as Style from './Styles';
+import { ReactComponent as GithubIcon } from '../../svg/github.svg';
+import { ReactComponent as GoogleIcon } from '../../svg/google.svg';
+import { ReactComponent as AppleIcon } from '../../svg/apple.svg';
+import { ReactComponent as KakaoIcon } from '../../svg/kakao.svg';
+import { ReactComponent as FacebookIcon } from '../../svg/facebook.svg';
+
+const socialIcon = {
+  github: <GithubIcon />,
+  google: <GoogleIcon />,
+  facebook: <FacebookIcon />,
+  kakao: <KakaoIcon />,
+  apple: <AppleIcon />,
+};
+
+export function SocialLoginButton({ social, ...props }) {
+  return (
+    <Style.SocialLoginButton social={social} {...props}>
+      {socialIcon[social]}
+    </Style.SocialLoginButton>
+  );
+}
+
+SocialLoginButton.propTypes = {
+  /**
+   * 소셜 선택
+   */
+  social: PropTypes.oneOf(['google', 'facebook', 'apple', 'kakao', 'github']).isRequired,
+  /**
+   * 클릭 이벤트
+   */
+  onClick: PropTypes.func,
+};
---
+++ frontend/src/components/SocialLoginButton/Styles.js
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components';
+
+const socialCss = {
+  kakao: css`
+    background-color: #FAE500;
+  `,
+  google: css`
+    background-color: #F8F8F8;
+  `,
+  github: css`
+    background-color: #3C4043;
+  `,
+  facebook: css`
+    background-color: #325CA6;
+  `,
+  apple: css`
+    background-color: #FFFFFF;
+  `,
+};
+
+export const SocialLoginButton = styled.button`
+  align-items: center;
+  border: 0;
+  border-radius: 4px;
+  box-shadow: 0 1px 2px 0;
+  display: flex;
+  height: 44px;
+  justify-content: center;
+  width:44px;
+  ${({ social }) => socialCss[social]}
+`;
