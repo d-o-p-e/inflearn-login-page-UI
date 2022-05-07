c47d7b068a81043d8eab3a87d4a6ceca0e06b5d4
주기 <kangju2000@naver.com>
2022-05-07 20:19:43 +0900

    :lipstick: 회원가입 page 생성 (#25)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Register from './pages/Register/Register';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Main />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Register/RegisterCss.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const RegisterForm = styled.form`
+  h2 {
+    margin-bottom: 0;
+  }
+  h4 {
+    margin-top: 0.5rem;
+    margin-bottom: 2rem;
+  }
+  margin: 0 auto;
+  padding-top: 3rem;
+  width: 350px;
+  text-align: center;
+`;
